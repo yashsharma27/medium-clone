e12128020c2954968785a7dc03462f91309a5dfd
Yash Sharma <yash.s@decisionpoint.local>
2024-09-02 00:02:05 +0530

    removed unessential code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,27 +16,4 @@ app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
 
-app.use('/api/v1/blog/*', async (c, next) => {
-  //get the header
-  // verify header
-  // if the header is correct, we can proceed
-  //if not, return 403
-
-  
-  const header = c.req.header("authorization") || "";
-  const token = header.split(" ")[1]
-  const response = await verify(header, c.env.JWT_SECRET)
-
-  if(response.id){
-    next()
-  }
-  else {
-    c.status(403)
-    return c.json({error: "unauthorized"})
-  }
-
-  await next()
-})
-
-
 export default app
