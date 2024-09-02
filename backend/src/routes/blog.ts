a0e9110bec42a62c82fca5bf9474868a2940e73e
Yash Sharma <yash.s@decisionpoint.local>
2024-09-02 22:17:23 +0530

    try catch added

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -16,17 +16,25 @@ export const blogRouter = new Hono<{
 
 blogRouter.use("/*", async (c, next) => {
     const authHeader = c.req.header("authorization") || "";
-    const user = await verify(authHeader, c.env.JWT_SECRET)
-    if(user){
-        c.set("userId" , user.id);
-        await next();
-    }
-    else {
+    try {
+        const user = await verify(authHeader, c.env.JWT_SECRET)
+        if(user){
+            c.set("userId" , user.id);
+            await next();
+        }
+        else {
+            c.status(403)
+            return c.json({
+                message : "You are not logged in"
+            })
+        }
+    } catch (error) {
         c.status(403)
         return c.json({
             message : "You are not logged in"
         })
     }
+    
 })
 
 blogRouter.post('/', async(c) => {
